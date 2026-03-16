835d555de21bd6243a310e39e0da40383e87496e
Sirclair <150465695+Sirclair@users.noreply.github.com>
2026-03-16 12:36:47 +0200

    Add template directory to Django settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -103,7 +103,7 @@ else:
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"], 
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
