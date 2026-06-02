06261221294a67e64e87abb66d08851f3bc43c1d
Sirclair <brighttaurai10@gmail.com>
2026-06-02 14:01:50 +0200

    Fix: add actual production vercel url to cors configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -183,6 +183,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://farm-management-omega-ten.vercel.app",
+    "https://farm-management-tawny-nine.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -210,6 +211,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
     "https://farm-management-omega-ten.vercel.app",
     "https://farm-management-8o37.onrender.com",
+    "https://farm-management-tawny-nine.vercel.app",
+    
 ]
 
 # --------------------------------------------------
