76b029d8d1de8d0af679d9593c1d260596eef5ab
Sirclair <brighttaurai10@gmail.com>
2026-06-04 15:29:45 +0200

    Create initial admin

--- backend/create_admin.py
+++ backend/create_admin.py
@@ -1,31 +1,11 @@
-import os
+from accounts.models import User
 
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-django.setup()
-
-from django.contrib.auth import get_user_model
-
-
-def sync_admin():
-    User = get_user_model()
-    # Pulling from Render Env Vars
-    username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
-    password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "FarmAdmin")
-
-    user, created = User.objects.get_or_create(username=username)
-
-    user.set_password(password)  # This updates the password if they exist
-    user.is_superuser = True
-    user.is_staff = True
-    user.save()
-
-    if created:
-        print(f"Admin {username} created.")
-    else:
-        print(f"Admin {username} password updated.")
-
-
-if __name__ == "__main__":
-    sync_admin()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="zonketasales@gmail.com",
+        password="Admin@12345"
+    )
+    print("Superuser created")
+else:
+    print("Superuser already exists")
